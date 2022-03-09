d1ee332b5b89b8f2e81ac4ec1c5cf7bfba8e42f6
Atharva Vitekar <86305350+atharva9797@users.noreply.github.com>
2022-03-09 20:53:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Kleiba </title>
+<link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+
+	<div class="container">
+		<div class="error"></div>
+		<form id="frm-mobile-verification">
+			<div class="form-heading">Mobile Number Verification</div>
+
+			<div class="form-row">
+				<input type="number" id="mobile" class="form-input"
+					placeholder="Enter the 10 digit mobile">
+			</div>
+
+			<input type="button" class="btnSubmit" value="Send OTP"
+				onClick="sendOTP();">
+		</form>
+	</div>
+	<script src="verification.js"></script>
+</body>
+</html>
